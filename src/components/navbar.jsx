22a5c4c1254c9433b3b322cfde92c83b0dd3076d
merinxavier <145212362+merinxavier@users.noreply.github.com>
2024-07-06 12:35:19 +0530

    Add files via upload

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
 function NavBar(){
     return (
-        <div style={{display:'flex',justifyContent:'space-between',border:'1px solid darkblue',padding:'20px',height:'50px',borderRadius:'20px'}}>
-      <h1>Jyothi</h1>
-      <button style={{backgroundColor:'lavender',borderRadius:'10px',height:'40px'}}>Login</button>
+        <div style={{display:'flex',justifyContent:'space-between',backgroundColor:'blue',border:'1px solid darkblue',padding:'20px',height:'50px',borderRadius:'20px',fontsize:'x-large'}}>
+      <h1 style={{fontweight:'bold',fontstyle:'bold',color:'white',fontsize:'60px'}}>Jyothi</h1>
+      <button style={{backgroundColor:'lavender',borderRadius:'10px',width:'90px',height:'40px',display:'flex',justifyContent:'center',alignItems:'center'}}>Login</button>
+      
     </div>
     )
 }
-
 
 export default NavBar;
